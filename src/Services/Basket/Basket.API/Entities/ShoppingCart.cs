00b64ec1ae2eb9f3d731f7788ef44403690d7bf7
tadeu <tadeu@paneas.com>
2022-08-21 19:19:13 -0300

    Add basket MS

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿namespace Basket.API.Entities;
+
+public class ShoppingCart
+{
+    public string? UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; }
+
+    public ShoppingCart()
+    {
+        Items ??= new List<ShoppingCartItem>();
+    }
+    public ShoppingCart(string userName)
+    {
+        Items ??= new List<ShoppingCartItem>();
+        UserName = userName;
+    }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal totalprice = 0;            
+            foreach (var item in Items)
+            {
+                totalprice += item.Price * item.Quantity;
+            }
+            return totalprice;
+        }
+    }
+
+
+}
